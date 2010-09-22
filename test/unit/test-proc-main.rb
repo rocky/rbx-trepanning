f6c206587c45731b234a96115d745ee4dd47458b
rocky <rockyb@rubyforge.org>
2010-09-22 08:05:09 -0400

    Unit tests for processor/main.rb and processor/validate.rb

---
+++ test/unit/test-proc-main.rb
@@ -0,0 +1,101 @@
+#!/usr/bin/env ruby
+require 'test/unit'
+require 'rubygems'; require 'require_relative'
+require_relative '../../processor/main'
+## require_relative '../../app/core'
+
+# Mock debugger stub. FIXME: put in comment helper routine.
+class Trepan
+end
+
+# Test Trepan:CmdProcessor
+class TestCmdProcessor < Test::Unit::TestCase
+
+  def setup
+    @dbg     = Trepan.new
+    ## @core    = Trepan::Core.new(@dbg)
+    ## @cmdproc = @core.processor = Trepan::CmdProcessor.new(@core)
+    @cmdproc = Trepan::CmdProcessor.new(self)
+    @cmds    = @cmdproc.instance_variable_get('@commands')
+  end
+
+  # See that we have can load up commands
+  def test_command_load
+    assert @cmds.is_a?(Hash), 'Should have gotten a command hash'
+    assert @cmds.keys.size > 0, 'Should have found at least one command'
+    cmd_name, cmd_obj = @cmds.first
+    assert cmd_name.is_a?(String), 'Should have string name for a command'
+    assert(cmd_obj.kind_of?(Trepan::Command), 
+           'Command should be a Trapan::Command')
+    assert cmd_obj.respond_to?(:run), 'Command should have a run method'
+  end
+
+  def no__test_compute_prompt
+    assert_equal('(rbdbgr): ', @cmdproc.compute_prompt)
+    Thread.new{ assert_equal('(rbdbgr@55): ', @cmdproc.compute_prompt.gsub(/@\d+/, '@55'))}.join
+    x = Thread.new{ Thread.pass; x = 1 }
+    assert_equal('(rbdbgr@main): ', @cmdproc.compute_prompt)
+    x.join
+    @cmdproc.debug_nest += 1
+    assert_equal('((rbdbgr)): ', @cmdproc.compute_prompt)
+    @cmdproc.debug_nest -= 1
+  end
+
+  # See that each command has required constants defined.  Possibly in
+  # a strongly-typed language we wouldn't need to do much of this.
+  def test_command_class_vars
+    @cmds.each do |cmd_name, cmd|
+      %w(HELP CATEGORY NAME SHORT_HELP).each do
+        |const|
+        value = cmd.class.const_get(const)
+        assert(value.is_a?(String),
+               "#{const} in command #{cmd_name} should be a String; got #{value}.")
+      end
+      %w(MIN_ARGS MAX_ARGS).each do
+        |const|
+        value = cmd.class.const_get(const)
+        assert(value.is_a?(Fixnum) || value.nil?,
+               "#{const} in command #{cmd_name} should be a Fixnum or nil; got #{value}.")
+      end
+      %w(ALIASES).each do
+        |const|
+        next unless cmd.class.constants.member?(:ALIASES)
+        ary = cmd.class.const_get(const)
+        assert(ary.is_a?(Array),
+               "#{const} in command #{cmd_name} should be an Array")
+
+        ary.each do |v| 
+          assert(v.is_a?(String),
+                 "#{const} in command #{cmd_name} should be Array of Strings; got #{v}.")
+        end
+      end
+    end
+  end
+
+  def no__test_run_command
+    def run_and_check(cmd, expect_msgs, expect_errmsgs, add_msg)
+      $errs = []; $msgs = []
+      @cmdproc.run_command(cmd)
+      assert_equal(expect_msgs, $msgs, 
+                   "#{add_msg}: mismatched messages from #{caller[0]}")
+      assert_equal(expect_errmsgs, $errs, 
+                   "Mismatched error messages from #{caller[0]}")
+    end
+    def @cmdproc.msg(mess)
+      $msgs << "#{mess}"
+    end
+    def @cmdproc.errmsg(mess)
+      $errs << "#{mess}"
+    end
+    run_and_check('!s=1', ['D=> 1'], [], "! evaluation")
+    run_and_check('print "foo"', ['foo'], [], "print command")
+    run_and_check('set autoeval off', 
+                  ['Evaluation of unrecognized debugger commands is off.'], [], 
+                  "autoeval set")
+    run_and_check('asdf', [], 
+                  ['Undefined command: "asdf". Try "help".'], 
+                  "invalid command")
+    
+  end
+
+end
---
+++ test/unit/test-proc-validate.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+require 'test/unit'
+require 'rubygems'; require 'require_relative'
+require_relative '../../processor/main' # Have to include before validate!
+                                        # FIXME
+require_relative '../../processor/validate'
+require_relative '../../app/mock'
+
+$errors = []
+$msgs   = []
+
+# Test Trepan::CmdProcessor Validation portion
+class TestValidate < Test::Unit::TestCase
+
+  def setup
+    $errors = []
+    $msgs   = []
+    @proc    = Trepan::CmdProcessor.new(Trepan::MockCore.new())
+
+    class << @proc
+      def errmsg(msg)
+        $errors << msg
+      end
+      def print_location
+        # $msgs << "#{@frame.source_container} #{@frame.source_location[0]}"
+        $msgs << "#{@frame.source_container} "
+        # puts $msgs
+      end
+    end
+  end
+
+  # See that we have can load up commands
+  def test_basic
+    onoff = 
+    [['1', true],  ['on', true],
+     ['0', false], ['off', false]].each do |arg, expected|
+      assert_equal(expected, @proc.get_onoff(arg))
+    end
+    [['1', 1],  ['1E', nil], ['bad', nil], ['1+1', 2], ['-5', -5]].each do 
+      |arg, expected|
+      assert_equal(expected, @proc.get_int_noerr(arg))
+    end
+  end
+
+  def no_test_breakpoint_position
+    require 'thread_frame'
+    tf = RubyVM::ThreadFrame.current
+    @proc.frame_setup(tf)
+
+    def munge(args)
+      args[1] = 'bogus'
+      args
+    end
+
+    assert_equal([0, 'bogus', true, 'true', nil],
+                 munge(@proc.breakpoint_position(%w(O0))))
+    assert_equal([1, 'bogus', false, 'true', nil], 
+                 munge(@proc.breakpoint_position(%w(1))))
+    assert_equal([2, 'bogus', false, 'a > b', nil],
+                 munge(@proc.breakpoint_position(%w(2 if a > b))))
+  end
+
+  def test_int_list
+    assert_equal([1,2,3], @proc.get_int_list(%w(1+0 3-1 3)))
+    assert_equal(0, $errors.size)
+    assert_equal([2,3], @proc.get_int_list(%w(a 2 3)))
+    assert_equal(1, $errors.size)
+  end
+
+  def no__test_method?
+    def foo; 5 end
+    
+    # require_relative '../../lib/rbdbgr'
+    # dbgr = Trepan.new(:set_restart => true)
+    # FIXME: 'foo', 'errmsg'
+    ['Array#map', 'Trepan::CmdProcessor.new'
+    ].each do |str|
+      # dbgr.debugger if 'foo' == str
+      assert_equal(true, @proc.method?(str),
+                   "#{str} should be known as a method")
+    end
+    ['food', '.errmsg'
+    ].each do |str|
+      # dbgr.debugger if 'foo' == str
+      assert_equal(false, @proc.method?(str),
+                   "#{str} should be known as a method")
+    end
+
+  end
+
+end
