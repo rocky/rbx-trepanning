9d92d3312db223110534138eca49e95ea04d7ebf
rocky <rockyb@rubyforge.org>
2010-12-15 19:26:25 -0500

    Add minimal -Xdebug integration test

--- test/integration/test-quit.rb
+++ test/integration/test-quit.rb
@@ -6,7 +6,20 @@ require_relative 'helper'
 class TestQuit < Test::Unit::TestCase
   @@NAME = File.basename(__FILE__, '.rb')[5..-1]
 
-  def test_it
+  def test_trepanx_call
     assert_equal(true, run_debugger(@@NAME, 'null.rb'))
+  end
+
+  def test_xcode_call
+    no_error = run_debugger('quit-Xdebug', 'null.rb',
+                            {:xdebug => true,
+                              :short_cmd => @@NAME,
+                              :do_diff => false
+                            })
+    assert_equal(true, no_error)
+    if no_error
+      outfile = File.join(File.dirname(__FILE__), 'quit-Xdebug.out')
+      FileUtils.rm(outfile)
+    end
   end
 end
