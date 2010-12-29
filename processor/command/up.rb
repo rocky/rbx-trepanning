5a9aeba98a635c8fc22203f4ae5e8c60e5b0b9fb
brainopia <ravwar@gmail.com>
2010-12-30 02:23:50 +0300

    Remove unneeded require with explicit location

--- processor/command/up.rb
+++ processor/command/up.rb
@@ -1,17 +1,16 @@
 # Copyright (C) 2010 Rocky Bernstein <rockyb@rubyforge.net>
 require 'rubygems'; require 'require_relative'
-require '/home/rocky-rvm/.rvm/src/rbx-trepanning/processor/command/base/cmd'
 require_relative 'base/cmd'
 
 # up command. Like 'down' butthe direction (set by DIRECTION) is different.
 #
-# NOTE: The down command  subclasses this, so beware when changing! 
+# NOTE: The down command  subclasses this, so beware when changing!
 class Trepan::Command::UpCommand < Trepan::Command
 
   # Silence already initialized constant .. warnings
-  old_verbose = $VERBOSE  
+  old_verbose = $VERBOSE
   $VERBOSE    = nil
-  HELP        = 
+  HELP        =
 "u(p) [count]
 
 Move the current frame up in the stack trace (to an older frame). 0 is
@@ -26,14 +25,14 @@ See also 'down' and 'frame'.
   NAME          = File.basename(__FILE__, '.rb')
   NEED_STACK    = true
   SHORT_HELP    = 'Move frame in the direction of the caller of the last-selected frame'
-  $VERBOSE      = old_verbose 
+  $VERBOSE      = old_verbose
 
   def initialize(proc)
     super
     @direction = +1 # -1 for down.
   end
 
-  # Run 'up' command. 
+  # Run 'up' command.
   def run(args)
 
     # FIXME: move into @proc and test based on NEED_STACK.
@@ -49,7 +48,7 @@ See also 'down' and 'frame'.
       count_str = args[1]
       name_or_id = args[1]
       opts = {
-        :msg_on_error => 
+        :msg_on_error =>
         "The '#{NAME}' command argument must eval to an integer. Got: %s" % count_str,
         :min_value => -@proc.stack_size,
         :max_value => @proc.stack_size-1
@@ -68,19 +67,19 @@ if __FILE__ == $0
 
   # def sep ; puts '=' * 40 end
   # cmd.run [cmd.name]
-  # %w(-1 0 1 -2).each do |count| 
+  # %w(-1 0 1 -2).each do |count|
   #   puts "#{cmd.name} #{count}"
   #   cmd.run([cmd.name, count])
-  #   sep 
+  #   sep
   # end
   # def foo(cmd, cmd.name)
   #   puts "#{cmd.name}"
   #   cmd.run([cmd.name])
   #   sep
-  #   %w(-2 -1).each do |count| 
+  #   %w(-2 -1).each do |count|
   #     puts "#{cmd.name} #{count}"
   #     cmd.run([cmd.name, count])
-  #     sep 
+  #     sep
   #   end
   # end
   # foo(cmd, cmd.name)
