915d5c2439622a86a790fa3ed17720bfe2faf9cb
rocky <rockyb@rubyforge.org>
2011-02-08 23:21:47 -0500

    eval without a string eval's the current source code. Update and expand 'eval' help text.

--- processor/command/eval.rb
+++ processor/command/eval.rb
@@ -7,18 +7,37 @@ class Trepan::Command::EvalCommand < Trepan::Command
 
   CATEGORY      = 'data'
   HELP    = <<-HELP
+eval [STRING]
+
 Run code in the context of the current frame.
 
 The value of the expression is stored into a global variable so it
 may be used again easily. The name of the global variable is printed
 next to the inspect output of the value.
+
+If no string is given we run the string from the current source code
+about to be run
+
+eval 1+2  # 3
+eval @v
+eval      # Run current source-code line
+
+See also 'set autoeval'
       HELP
 
   NAME          = File.basename(__FILE__, '.rb')
   NEED_STACK    = true
   SHORT_HELP    = 'Run code in the current context'
   def run(args)
-    @proc.debug_eval(@proc.cmd_argstr, settings[:maxstring])
+    if args.size == 1
+      loc = @proc.source_location_info
+      opts = {:reload_on_change => @proc.reload_on_change}
+      loc, junk, text = @proc.loc_and_text(loc, opts)
+      msg "eval: #{text}"
+    else
+      text = @proc.cmd_argstr
+    end
+    @proc.eval_code(text, settings[:maxstring])
   end
 end
 
@@ -26,8 +45,6 @@ if __FILE__ == $0
   require_relative '../mock'
   dbgr, cmd = MockDebugger::setup
   arg_str = '1 + 2'
-  ## cmd.proc.instance_variable_set('@cmd_argstr', arg_str)
-  dbgr.instance_variable_set('@cmd_argstr', arg_str)
-
-  ## cmd.run([cmd.name, arg_str])
+  cmd.proc.instance_variable_set('@cmd_argstr', arg_str)
+  puts "eval #{arg_str} is: #{cmd.run([cmd.name, arg_str])}"
 end
