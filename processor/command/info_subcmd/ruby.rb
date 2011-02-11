b1c65c0b6413930a087f09fa6e7eb7a1eb51aa53
rocky <rockyb@rubyforge.org>
2011-02-11 15:45:25 -0500

    info rubinius -> info ruby. To be more compatible with rb-trepanning

--- processor/command/info_subcmd/ruby.rb
+++ processor/command/info_subcmd/ruby.rb
@@ -3,18 +3,21 @@
 require 'rubygems'; require 'require_relative'
 require_relative '../base/subcmd'
 
-class Trepan::Subcommand::InfoRubinius < Trepan::Subcommand
+class Trepan::Subcommand::InfoRuby < Trepan::Subcommand
   unless defined?(HELP)
     Trepanning::Subcommand.set_name_prefix(__FILE__, self)
     HELP         = <<-EOH
 #{CMD=PREFIX.join(' ')} [-v|--verbose|-no-verbose]
 
 Show Rubinius version information such as you'd get from 
-"rbx -v" or "rbx -vv"
+"rbx -v" (which is really just the value of RUBY_RELEASE_DATE
+or "rbx -vv". 
+
+See also constants: RUBY_DESCRIPITON, RUBY_VERSION, and RUBY_RELEASE_DATE."
     EOH
     MIN_ABBREV   = 'ru'.size
     NEED_STACK   = false
-    SHORT_HELP   = 'Rubinius version information'
+    SHORT_HELP   = 'Ruby version information'
    end
 
   def parse_options(args) # :nodoc
@@ -32,7 +35,7 @@ Show Rubinius version information such as you'd get from
 
   def run(args)
     options = parse_options(args[2..-1])
-    msg Rubinius.version
+    msg RUBY_DESCRIPTION
     if options[:verbose]
       msg "Options:"
       msg "  Interpreter type: #{Rubinius::INTERPRETER}"
@@ -50,7 +53,7 @@ if __FILE__ == $0
   # Demo it.
   $0 = __FILE__ + 'notagain' # So we don't run this again
   require_relative '../../mock'
-  cmd = MockDebugger::sub_setup(Trepan::Subcommand::InfoRubinius, false)
+  cmd = MockDebugger::sub_setup(Trepan::Subcommand::InfoRuby, false)
   cmd.run(cmd.prefix)
   %w(-v --verbose --no-verbose).each do |opt|
     puts '-' * 10 + " #{opt}"
