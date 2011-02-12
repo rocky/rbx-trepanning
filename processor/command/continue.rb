ea001a1c5fefe404e0179316c785f9d455ddd8fd
rocky <rockyb@rubyforge.org>
2011-02-12 16:23:28 -0500

    Sync continue somewhat with rb-trepanning

--- processor/command/continue.rb
+++ processor/command/continue.rb
@@ -1,31 +1,38 @@
+# Copyright (C) 2011 Rocky Bernstein <rockyb@rubyforge.net>
 require 'rubygems'; require 'require_relative'
 require_relative 'base/cmd'
 require_relative '../stepping'
 
 class Trepan::Command::ContinueCommand < Trepan::Command
-  NAME         = File.basename(__FILE__, '.rb')
-  HELP         = <<-HELP
+  unless defined?(HELP)
+    NAME = File.basename(__FILE__, '.rb')
+    HELP = <<-HELP
 #{NAME} [breakpoint-position]
 
 Leave the debugger loop and continue execution. Subsequent entry to
 the debugger however may occur via breakpoints or explicit calls, or
 exceptions.
 
-If a parameter is given, a one-time breakpoint is set at that position
-before continuing. 
+If a parameter is given, a temporary breakpoint is set at that position
+before continuing. Offset are numbers prefixed with an "O" otherwise
+the parameter is taken as a line number.
 
 Examples:
    #{NAME}
    #{NAME} 10    # continue to line 10
-   #{NAME} Array#map # beginning of next Array#map call
+   #{NAME} o20   # continue to VM Instruction Sequence offset 20
+   #{NAME} gcd   # continue to first instruction of method gcd
+   #{NAME} IRB.start o7 # continue to IRB.start offset 7
 
 See also 'step', 'next', 'finish', and 'nexti' commands.
-      HELP
-  ALIASES      = %w(c cont)
-  CATEGORY     = 'running'
-  MAX_ARGS     = 1
-  NEED_RUNNING = true
-  SHORT_HELP   = 'Continue execution of the debugged program'
+    HELP
+
+    ALIASES      = %w(c cont)
+    CATEGORY     = 'running'
+    MAX_ARGS     = 2  # Need at most this many
+    NEED_RUNNING = true
+    SHORT_HELP   = 'Continue execution of the debugged program'
+  end
 
   # This is the method that runs the command
   def run(args)
@@ -80,4 +87,5 @@ end
 if __FILE__ == $0
   require_relative '../mock'
   dbgr, cmd = MockDebugger::setup
+  p cmd.run([cmd.name])
 end
