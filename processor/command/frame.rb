39b7cae59015268bac74a1274901bdb35de8523b
rocky <rockyb@rubyforge.org>
2011-05-31 08:19:04 -0400

    Small change to make less Rubinius specific

--- processor/command/frame.rb
+++ processor/command/frame.rb
@@ -59,7 +59,7 @@ See also 'up', 'down', and  'backtrace'.
     #   ## @proc.find_and_set_debugged_frame(frame, thread_id)
     end
 
-    stack_size = @proc.dbgr.vm_locations.size
+    stack_size = @proc.frame.stack_size
     if stack_size == 0
       errmsg('No frames recorded.')
       return false
