7a95bc6482eacd51031afae837c025b273099af6
rocky <rockyb@rubyforge.org>
2011-06-02 20:04:13 -0400

    Sync with rb-trepanning and rb8-trepanning.

--- processor/command/tbreak.rb
+++ processor/command/tbreak.rb
@@ -4,11 +4,12 @@ require_relative 'break'
 
 class Trepan::Command::SetTempBreakpointCommand < 
     Trepan::Command::BreakCommand
-  ALIASES      = %w(tb tbrk)
+  ALIASES      = []
   CATEGORY     = 'breakpoints'
   NAME         = File.basename(__FILE__, '.rb')
   HELP         = <<-HELP
 Same as break, but the breakpoint is deleted when it is hit.
+See also "break".
       HELP
   SHORT_HELP   = 'Set a temporary breakpoint'
   
