d669efda1028a60d063fc259669a79405da66817
rocky <rockyb@rubyforge.org>
2011-09-17 20:51:36 -0400

    Grammar

--- processor/command/set_subcmd/different.rb
+++ processor/command/set_subcmd/different.rb
@@ -9,7 +9,7 @@ class Trepan::Subcommand::SetDifferent < Trepan::SetBoolSubcommand
     HELP         = <<-EOH
 #{PREFIX.join(' ')} [on|off|nostack]
 
-Set to make sure 'next/step' move to a new position.
+Set to make sure 'next' or 'step' moves to a new position.
 
 Due to the interpretive, expression-oriented nature of the Ruby
 Language and implementation, each line often may contain many possible
