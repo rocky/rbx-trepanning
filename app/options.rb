d9694416aea460e64fa1483a5045eafbd82a2f9b
rocky <rockyb@rubyforge.org>
2013-07-18 22:30:25 -0400

    Get ready for release 0.2.1

--- app/options.rb
+++ app/options.rb
@@ -9,7 +9,7 @@ class Trepan
   require 'rubygems'; require 'require_relative'
   require_relative 'default'
 
-  VERSION = '0.2.1dev'
+  VERSION = '0.2.1'
   PROGRAM = 'trepanx' unless defined? Trepan::PROGRAM
 
   def self.show_version
